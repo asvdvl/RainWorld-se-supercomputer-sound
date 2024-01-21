490caeb5ac6e6084d87b4d3d547a7e0103e1e4f6
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-01-22 00:14:44 +0300

    an attempt to exclude pulling out all objects by storing coordinates in a table

--- prepare-to-modportal.sh
+++ prepare-to-modportal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm ./RainWorld-se-supercomputer-sound_0.2.1.zip
 cd ..
-zip -r ./RainWorld-se-supercomputer-sound_0.2.1.zip ./RainWorld-se-supercomputer-sound -x "*.git/*" "*.7z" "*.aup3" "*.kra" "*.sh" "*.gitignore"
+rm ./RainWorld-se-supercomputer-sound_0.3.0.zip
+zip -r ./RainWorld-se-supercomputer-sound_0.3.0.zip ./RainWorld-se-supercomputer-sound -x "*.git/*" "*.7z" "*.aup3" "*.kra" "*.sh" "*.gitignore"
+cp ./RainWorld-se-supercomputer-sound_0.3.0.zip ~/.factorio/mods/
