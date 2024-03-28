d176c5403614ec9ed825f56baf8927a992a59e90
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-03-28 15:39:23 +0300

    moved to fmtk packing tool

--- prepare-to-modportal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-rm ./RainWorld-se-supercomputer-sound_0.3.0.zip
-zip -r ./RainWorld-se-supercomputer-sound_0.3.0.zip ./RainWorld-se-supercomputer-sound -x "*.git/*" "*.7z" "*.aup3" "*.kra" "*.sh" "*.gitignore"
-cp ./RainWorld-se-supercomputer-sound_0.3.0.zip ~/.factorio/mods/
