99a5a002aaebeed3c990c830495b0d7d5ee8bfe1
asvdvl <57688070+asvdvl@users.noreply.github.com>
2024-06-30 06:45:33 +0300

    delet old settings

--- prototypes/se-supercomputer-change-sound.lua
+++ prototypes/se-supercomputer-change-sound.lua
@@ -5,11 +5,11 @@ for _, prototype in pairs(data.raw["assembling-machine"]) do
     local _, _, proto_name, proto_number = string.find(prototype.name, "(.+)-(%d+)$")
     if proto_name and proto_name == "assembling-machine" then --se-space-supercomputer
         work_sound = file_prefix.."4.ogg"
-        
+
         proto_number = (proto_number and tonumber(proto_number)) or 0
         if proto_number >= 1 and proto_number <= 4 then
             work_sound = file_prefix..tostring(proto_number)..".ogg"
         end
-        fssm.applyNewSound(prototype, work_sound)
+        fssm.applyNewSound(prototype, work_sound, settings.startup["RWsss_volume"].value)
     end
 end
--- settings.lua
+++ settings.lua
@@ -1,39 +1,10 @@
 data:extend({
     {
         type = "double-setting",
-        name = "volume",
+        name = "RWsss_volume",
         setting_type = "startup",
-        default_value = 0.7
-    },
-    {
-        type = "int-setting",
-        name = "fade_in_ticks",
-        setting_type = "startup",
-        default_value = 60,
+        default_value = 0.7,
         minimum_value = 0,
-        maximum_value = 4294967295
-    },
-    {
-        type = "int-setting",
-        name = "fade_out_ticks",
-        setting_type = "startup",
-        default_value = 60,
-        minimum_value = 0,
-        maximum_value = 4294967295
-    },
-    {
-        type = "bool-setting",
-        name = "use_doppler_shift",
-        setting_type = "startup",
-        default_value = false
-    },
-    {
-        type = "int-setting",
-        name = "default-max_sounds_per_type",
-        setting_type = "startup",
-        default_value = 1,
-        minimum_value = 0,
-        maximum_value = 255
-    },
-    
+        maximum_value = 1
+    }
 })
