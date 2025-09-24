786da4b776ec81bf08869755c09f03e2099bd467
asvdvl <57688070+asvdvl@users.noreply.github.com>
2025-09-24 20:59:36 +0300

    ported to 2.0

--- prototypes/se-supercomputer-change-sound.lua
+++ prototypes/se-supercomputer-change-sound.lua
@@ -3,7 +3,7 @@ local file_prefix = "__RainWorld-se-supercomputer-sound__/sound/rw-randomGods-sc
 
 for _, prototype in pairs(data.raw["assembling-machine"]) do
     local _, _, proto_name, proto_number = string.find(prototype.name, "(.+)-(%d+)$")
-    if proto_name and proto_name == "assembling-machine" then --se-space-supercomputer
+    if proto_name and proto_name == "se-space-supercomputer" then --assembling-machine for tests
         work_sound = file_prefix.."4.ogg"
 
         proto_number = (proto_number and tonumber(proto_number)) or 0
